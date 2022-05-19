c4f8cb5e73c42cbefff0bbb0020d3dedbdea18cf
Khoi <kdng96@gmail.com>
2022-05-19 00:58:06 -0700

    log out button css

--- react-app/src/components/Videos/index.js
+++ react-app/src/components/Videos/index.js
@@ -22,33 +22,33 @@ const Videos = () => {
             <div className="allVideos">
                 {videoData.map((video, idx) => (
 
+                    <a href={`/videos/${video.id}`} className="vidLink">
+                        <div className="allVidsContainer">
 
-                    <div className="allVidsContainer">
 
+                            <div>
+                                <div className="createdInfo">
+                                    <p className="contentCreator">@{video.user.username} posted:</p>
+                                    <p className="dateCreated">{video?.created_at?.split(" ").slice(0, 4).join(" ")}</p>
+                                </div>
 
-                        <div>
-                            <div className="createdInfo">
-                                <p className="contentCreator">@{video.user.username} posted:</p>
-                                <p className="dateCreated">{video?.created_at?.split(" ").slice(0, 4).join(" ")}</p>
+                                <div className='videoContainer'>
+                                    <a href={`/videos/${video.id}`} className="vidLinkVideo">
+                                        <ReactPlayer url={`${video?.uploadFile}`} controls width='110%' height='615px' className="oneVideo" />
+                                    </a>
+                                </div>
                             </div>
 
-                            <div className='videoContainer'>
-                                <a href={`/videos/${video.id}`} className="vidLinkVideo">
-                                    <ReactPlayer url={`${video?.uploadFile}`} controls width='110%' height='615px' className="oneVideo" />
-                                </a>
-                            </div>
-                        </div>
 
+                            <div className="videoDetails">
 
-                        <div className="videoDetails">
-                            <a href={`/videos/${video.id}`} className="vidLink">
                                 <p className="videoTitle">{video?.title}</p>
-                            </a>
-                            <p className="videoDescription">{video?.description}</p>
+
+                                <p className="videoDescription">{video?.description}</p>
+                            </div>
+
                         </div>
-
-                    </div>
-
+                    </a>
 
                 ))}
             </div>
