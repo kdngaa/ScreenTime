ade92b8de75ef79403698dffbc48c3389a96142b
Khoi <kdng96@gmail.com>
2022-06-04 01:06:26 -0700

    mapping over videos Object

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,1 +1,20 @@
 import './seach.css'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+
+const Search = () => {
+    const history = useHistory()
+    const videos = useSelector((state) => state.videos)
+    const vids = Object.values(videos).map((video) => [
+        video.title,
+        video.description,
+        video.id,
+    ])
+
+
+
+
+
+
+}
