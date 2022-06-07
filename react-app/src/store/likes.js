cd8fdc7ea7ecb73ff7e76b024b7583265c7ba7cb
Khoi <kdng96@gmail.com>
2022-06-07 14:17:12 -0700

    action thunk created for post, load, and remove likes

--- react-app/src/store/likes.js
+++ react-app/src/store/likes.js
@@ -5,3 +5,63 @@ const ADD_LIKE = 'likes/ADD_LIKE'
 const REMOVE_LIKE = 'likes/REMOVE_LIKE'
 
 
+const load = likes => ({
+    type: LOAD,
+    likes
+});
+
+
+const postLike = like => ({
+    type: ADD_LIKE,
+    like
+});
+
+const deleteLike = like => ({
+    type: REMOVE_LIKE,
+    like
+});
+
+
+
+export const loadLikesThunk = () => async dispatch => {
+    const response = await fetch(`/api/likes/`, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        const likes = await response.json()
+        dispatch(load(likes))
+    }
+}
+
+
+
+export const postLikeThunk = id => async dispatch => {
+    const response = await fetch(`/api/likes/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            videoId: id
+        })
+    })
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(postLike(data))
+    }
+}
+
+
+
+export const deleteLikeThunk = id => async dispatch => {
+    const response = await fetch(`/api/likes/${id}`, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+        dispatch(deleteLike(id))
+    }
+}
+
+
