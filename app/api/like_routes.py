be97cfc7d266ad82d322fc5088bc7bda353dca7a
Khoi <kdng96@gmail.com>
2022-06-07 14:02:30 -0700

    post and get likes routes backend added

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -5,3 +5,31 @@ from datetime import datetime
 
 
 like_routes = Blueprint('likes', __name__)
+
+#GET ALL LIKES
+@like_routes.route('/', methods=['GET'])
+# @login_required
+def get_likes():
+  likes = Like.query.all()
+  print(likes,'<============')
+  return { 'likes': [ like.to_dict() for like in likes] }
+
+
+
+
+#POST LIKES
+@like_routes.route('/', methods=['POST'])
+@login_required
+def post_likes():
+
+  videoId = request.json['videoId']
+
+  new_like = Like(
+    userId = current_user.id,
+    videoId = videoId,
+  )
+
+  db.session.add(new_like)
+  db.session.commit()
+
+  return new_like.to_dict()
