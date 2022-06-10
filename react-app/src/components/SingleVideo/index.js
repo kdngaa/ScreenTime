b48f5180bf64547fba4ff6d7cab7b45b41fba87d
Khoi <kdng96@gmail.com>
2022-06-09 17:44:09 -0700

    update

--- react-app/src/components/SingleVideo/index.js
+++ react-app/src/components/SingleVideo/index.js
@@ -116,7 +116,7 @@ const SingleVideo = () => {
                         <p className="descriptionContent">{`${video.description}`}</p>
                             <div className="likeSection">
                                 {like ? (
-                                    <button  className="likeBtn" onClick={handleLike}><img className="likeImg" src="https://res.cloudinary.com/dv3gxfdon/image/upload/v1654819759/love_wj07gk.png"></img></button>
+                                    <button  className="likeBtn" onClick={handleLike}><img className="likeImg" src="https://res.cloudinary.com/dv3gxfdon/image/upload/v1654821741/love_2_kkrldq.png"></img></button>
                                     ) : (
                                     <button className="likeBtn" onClick={handleLike}><img className="likeImg" src="https://res.cloudinary.com/dv3gxfdon/image/upload/v1654819759/love_1_hphfcn.png"></img></button>
                                 )}
