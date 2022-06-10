35cf609b62a97a29f9b31e6d851b1baaf13e249d
Khoi <kdng96@gmail.com>
2022-06-09 17:16:46 -0700

    update like button

--- react-app/src/components/SingleVideo/index.js
+++ react-app/src/components/SingleVideo/index.js
@@ -116,9 +116,9 @@ const SingleVideo = () => {
                         <p className="descriptionContent">{`${video.description}`}</p>
                             <div className="likeSection">
                                 {like ? (
-                                    <button className="likeBtn" onClick={handleLike}><img className="likeImg" src="https://res.cloudinary.com/dv3gxfdon/image/upload/v1654819759/love_1_hphfcn.png"></img></button>
-                                ) : (
                                     <button  className="likeBtn" onClick={handleLike}><img className="likeImg" src="https://res.cloudinary.com/dv3gxfdon/image/upload/v1654819759/love_wj07gk.png"></img></button>
+                                    ) : (
+                                    <button className="likeBtn" onClick={handleLike}><img className="likeImg" src="https://res.cloudinary.com/dv3gxfdon/image/upload/v1654819759/love_1_hphfcn.png"></img></button>
                                 )}
                                 <p className="likeNum">{likes?.length}</p>
                             </div>
