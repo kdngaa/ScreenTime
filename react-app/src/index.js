7831a14cd2f53e1150cc42bc0515c268a177fc84
Khoi <kdng96@gmail.com>
2022-06-18 16:55:59 -0700

    modal context created

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { ModalProvider } from "./context/Modal";
 
 const store = configureStore();
 
